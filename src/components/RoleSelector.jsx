f51c0046e6a684999f50f6ee269ab14fc42d55ae
crishuinca <cr.huinca@duouc.cl>
2026-05-11 03:30:04 -0400

    navbar por rol y selector mejorado

--- src/components/RoleSelector.jsx
+++ src/components/RoleSelector.jsx
@@ -1,19 +1,22 @@
 import { useRole } from '../hooks/useRole'
 
 function RoleSelector() {
-  const { rol, setRol, roles } = useRole()
+  const { currentRole, rol, roles, setRol } = useRole()
 
   return (
-    <label className="role-selector">
-      <span>Rol de demo</span>
-      <select value={rol} onChange={(e) => setRol(e.target.value)}>
-        {roles.map((item) => (
-          <option key={item.id} value={item.id}>
-            {item.label}
-          </option>
-        ))}
-      </select>
-    </label>
+    <div className="role-box">
+      <label className="role-selector">
+        <span>Rol actual</span>
+        <select value={rol} onChange={(e) => setRol(e.target.value)}>
+          {roles.map((item) => (
+            <option key={item.id} value={item.id}>
+              {item.label}
+            </option>
+          ))}
+        </select>
+      </label>
+      <small>{currentRole.description}</small>
+    </div>
   )
 }
 
